776f651e344e85515ebaffa47692d8505f7dafab
YUN <yunmyeonghun@gmail.com>
2022-03-27 16:38:53 +0900

    Initial commit

---
+++ src/main/java/com/example/springdatajpaadv/SpringDataJpaAdvApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpaadv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaAdvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaAdvApplication.class, args);
+    }
+
+}
